a545ad8787f499f4b63922c560e89d93cd1ae29f
Mathijs de Kruyf <mathijs.de.kruyf@audax.nl>
2021-12-02 10:06:40 +0100

    Solved 2_1

---
+++ src/Services/AdventOfCode2021_2_1.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+final class AdventOfCode2021_2_1 extends AbstractAdventOfCode
+{
+    /**
+     * @param string $year
+     * @param string $day
+     * @return bool
+     */
+    public function solve(string $year, string $day): bool
+    {
+        $puzzleInput = $this->getPuzzleInput($year, $day);
+        $this->logger->debug(sprintf('Found %d lines', count($puzzleInput)));
+        $horizontalPosition = 0;
+        $depth = 0;
+        foreach ($puzzleInput as $line) {
+            $vector = explode(' ', trim($line));
+            $direction = $vector[0];
+            $value = (int)$vector[1];
+            switch ($direction) {
+                case 'forward':
+                    $horizontalPosition += $value;
+                    break;
+                case 'up':
+                    $depth -= $value;
+                    break;
+                case 'down':
+                    $depth += $value;
+                    break;
+                default:
+                    throw new \InvalidArgumentException($line);
+            }
+        }
+        $this->logger->info(sprintf('Found %d horizontal position', $horizontalPosition));
+        $this->logger->info(sprintf('Found %d depth', $depth));
+        $this->logger->info(sprintf('Found %d multiplied', $horizontalPosition * $depth));
+
+        return true;
+    }
+}
